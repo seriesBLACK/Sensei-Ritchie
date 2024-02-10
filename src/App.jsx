5f99c670d577f916ea7a1cc0269b4af4d2fc9f4b
Abd ul Walee Talal <aboodtalal51@gmail.com>
2024-02-10 05:22:44 +0300

    make front page

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BackgroundBeams, BackgroundBeamsDemo } from "./components/BackgroundBeamsDemo";
+import { Lamp } from "./components/Lamp";
+import { TypewriterEffect } from "./components/TypewriterEffect";
+
+
+export default function App() {
+
+
+  return (
+
+
+    <div>
+
+      <Lamp />
+      {/* <BackgroundBeamsDemo /> */}
+
+
+    </div>
+
+
+
+
+
+  )
+}
