89a800d734418d4d6aa28ba02d728f716f5da222
Abd ul Walee Talal <aboodtalal51@gmail.com>
2024-02-10 05:51:37 +0300

    make floating navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
 import { BackgroundBeams, BackgroundBeamsDemo } from "./components/BackgroundBeamsDemo";
+import { FloatingNav } from "./components/FloatingNav";
 import { Lamp } from "./components/Lamp";
 import { TypewriterEffect } from "./components/TypewriterEffect";
-
+import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
 
 export default function App() {
-
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+      icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "About",
+      link: "/about",
+      icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+      icon: (
+        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
+    },
+  ];
 
   return (
 
 
-    <div>
-
+    <div className="h-[200vh]">
+      <FloatingNav navItems={navItems} />
       <Lamp />
       {/* <BackgroundBeamsDemo /> */}
 
 
     </div>
-
-
-
-
-
   )
 }
